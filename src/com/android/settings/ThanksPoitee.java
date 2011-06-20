594d288572bf3f72440f42b153fa8fa2ef8cc550
r2DoesInc <r2doesinc@gmail.com>
2011-06-20 06:00:24 -0400

    Update thanks.

--- src/com/android/settings/ThanksPoitee.java
+++ src/com/android/settings/ThanksPoitee.java
@@ -9,7 +9,7 @@ public class ThanksPoitee extends Activity {
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       tv.setText("-Free access to his personal quad core box to build on. Without him, I wouldnt be able to dev.");
+       tv.setText("-Free access to his personal quad core box to build on. Without him, I wouldnt be able to dev." + " \n-He came up with the idea for the OMFBOT and currently maintains that." );
        setContentView(tv);
    }
 }
